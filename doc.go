dea51c8996009508260a865bf8f11ed03bfad4a4
Arne Jørgensen <arne@arnested.dk>
2019-03-03 12:29:53 +0100

    Fix documentation

--- doc.go
+++ doc.go
@@ -3,8 +3,9 @@
 Package function is a Google Cloud Function receiving webhook events
 from DNSimple (https://dnsimple.com/webhooks).
 
-It reacts to `dnssec.rotation_complete` events and passes the new DS
-record on to DK Hostmaster via their DS Update protocol
+It reacts to `dnssec.rotation_start` and `dnssec.rotation_complete`
+events and passes the new DS record on to DK Hostmaster via their DS
+Update protocol
 (https://github.com/DK-Hostmaster/dsu-service-specification).
 
 The cloud function needs to be configured through environment variables.
@@ -12,6 +13,9 @@ The cloud function needs to be configured through environment variables.
 The `TOKEN` environment variable is the access token that should be
 added as URL query parameter to the trigger URL (e.g.
 `?token=abcdefeghijklmnopqrstuvxyz0123456789`).
+
+The `DNSIMPLE_TOKEN` environment variable is a DNSimple API token that
+is used to retrieve DS records from DNsimple.
 
 For the domains in your DNSimple account that you would like this
 cloud function to update in DK Hostmaster you need to add three
